81d7137d4d9e9fb9bc3c4e5469663cde61de114d
crink1 <1575460882@qq.com>
2023-07-22 20:47:54 +0800

    类和对象

---
+++ CPP_CODE/7_22.cpp
@@ -0,0 +1,30 @@
+//int main()
+//{
+//	int array[] = { 1, 2, 3, 4, 5 };
+//
+//	for (int i = 0; i < sizeof(array) / sizeof(array[0]); ++i)
+//		array[i] *= 2;
+//
+//	for (int i = 0; i < sizeof(array) / sizeof(array[0]); ++i)
+//		cout << array[i] << " ";
+//	cout << endl;
+//
+//
+//	for (auto& x : array)
+//	{
+//		x *= 2;
+//	}
+//
+//	// 范围for
+//	// 依次取数组中的数据赋值给e
+//	// 自定判断结束
+//	// 自动迭代
+//	for (auto e : array)
+//	{
+//		cout << e << " ";
+//	}
+//	cout << endl;
+//
+//	return 0;
+//}
+
