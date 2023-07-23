3f3e72aa3d7ee46e67ea0609eb39c84bf2ced0d6
crink1 <1575460882@qq.com>
2023-07-23 22:25:31 +0800

    类和对象

--- CPP_CODE/7_22.cpp
+++ CPP_CODE/7_22.cpp
@@ -27,4 +27,55 @@
 //
 //	return 0;
 //}
+//class Stack
+//{
+//public:
+//	// 成员函数
+//	void Init()
+//	{
+//		a = nullptr;
+//		top = capacity = 0;
+//	}
+//
+//	void Push(int x)
+//	{
+//		if (top == capacity)
+//		{
+//			size_t newcapacity = capacity == 0 ? 4 : capacity * 2;
+//			a = (int*)realloc(a, sizeof(int) * newcapacity);
+//			capacity = newcapacity;
+//		}
+//
+//		a[top++] = x;
+//	}
+//
+//	int Top()
+//	{
+//		return a[top - 1];
+//	}
+//
+//private:
+//	// 成员变量
+//	int* a;
+//	int top;
+//	int capacity;
+//};
+//
+//int main()
+//{
+//	Stack st;
+//	st.Init();
+//	st.Push(1);
+//	st.Push(2);
+//	st.Push(3);
+//	st.Push(4);
+//
+//	//cout << st.a[st.top-1] << endl;
+//
+//	cout << st.Top() << endl;
+//
+//
+//
+//	return 0;
+//}
 
---
+++ CPP_CODE/7_23.cpp
@@ -0,0 +1,141 @@
+#include<iostream>
+#include<assert.h>
+using namespace std;
+
+//class Date
+//{
+//public:
+//	void Init(int year, int month, int day)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//
+//	void Print()
+//	{
+//		cout << _year << "/" << _month << "/" << _day << endl;
+//	}
+//
+////private:
+//	int _year;   // 声明
+//	int _month;
+//	int _day;
+//};
+//
+//class A1 {
+//public:
+//	void f1() {}
+//private:
+//	char _ch;
+//	int _a;
+//};
+//
+//// 类中仅有成员函数
+//class A2 {
+//public:
+//	void f2() {}
+//};
+//
+//// 类中什么都没有---空类
+//class A3
+//{};
+
+
+//}int main()
+//{
+//	cout << sizeof(A1) << endl;
+//
+//	// 分配1byte
+//	cout << sizeof(A2) << endl;
+//	cout << sizeof(A3) << endl;
+//
+//	A2 aa2;
+//	A2 aaa2;
+//	cout << &aa2 << endl;
+//	cout << &aaa2 << endl;
+//
+//	cout << sizeof(Date) << endl;
+//	Date d1;
+//	Date d2;
+//
+//	d1.Print();
+//	d2.Print();
+//
+//	d1._year = 1;
+//	d2._year = 2;
+//
+//	return 0;
+//}int main()
+//{
+//	cout << sizeof(A1) << endl;
+//
+//	// 分配1byte，不存储数据，只是占位，表示对象存在过
+//	cout << sizeof(A2) << endl;
+//	cout << sizeof(A3) << endl;
+//
+//	A2 aa2;
+//	A2 aaa2;
+//	cout << &aa2 << endl;
+//	cout << &aaa2 << endl;
+//
+//	cout << sizeof(Date) << endl;
+//	Date d1;
+//	Date d2;
+//
+//	d1.Print();
+//	d2.Print();
+//
+//	d1._year = 1;
+//	d2._year = 2;
+//
+//	return 0;
+//}int main()
+//{
+//	cout << sizeof(A1) << endl;
+//
+//	// 分配1byte，不存储数据，只是占位，表示对象存在过
+//	cout << sizeof(A2) << endl;
+//	cout << sizeof(A3) << endl;
+//
+//	A2 aa2;
+//	A2 aaa2;
+//	cout << &aa2 << endl;
+//	cout << &aaa2 << endl;
+//
+//	cout << sizeof(Date) << endl;
+//	Date d1;
+//	Date d2;
+//
+//	d1.Print();
+//	d2.Print();
+//
+//	d1._year = 1;
+//	d2._year = 2;
+//
+//	return 0;
+//}int main()
+//{
+//	cout << sizeof(A1) << endl;
+//
+//	// 分配1byte，不存储数据，只是占位，表示对象存在过
+//	cout << sizeof(A2) << endl;
+//	cout << sizeof(A3) << endl;
+//
+//	A2 aa2;
+//	A2 aaa2;
+//	cout << &aa2 << endl;
+//	cout << &aaa2 << endl;
+//
+//	cout << sizeof(Date) << endl;
+//	Date d1;
+//	Date d2;
+//
+//	d1.Print();
+//	d2.Print();
+//
+//	d1._year = 1;
+//	d2._year = 2;
+//
+//	return 0;
+//}
