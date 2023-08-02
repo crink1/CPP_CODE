0145eccc50d4cf7666e094d4e4fb1620ffe4facc
crink1 <1575460882@qq.com>
2023-08-02 22:21:48 +0800

    string

--- CPP_CODE/7_31.cpp
+++ CPP_CODE/7_31.cpp
@@ -1,6 +1,6 @@
-//#include<iostream>
-//using namespace std;
-//
+#include<iostream>
+using namespace std;
+
 //inline int Add(int a, int b)
 //{
 //	return a + b;
@@ -27,3 +27,89 @@
 //	cout<<ret<<endl;
 //	return 0;
 //}
+
+//auto不能作为函数参数
+//void test(auto a)
+//{
+//
+//}
+
+//int main()
+//{
+//	auto a = 10;
+//	auto b = 'a';
+//	cout << typeid(a).name() << endl;
+//	cout << typeid(b).name() << endl;
+//
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	auto b = &a;
+//	auto c = &a;
+//	auto& d = a;
+//	cout << typeid(b).name() << endl;
+//    cout << typeid(c).name() << endl;
+//	cout << typeid(d).name() << endl;
+//	return 0;
+//}
+
+//int main()
+//{
+//	auto a = 0, b = 10;
+//	//auto c = 20, d = 114.514;
+//
+//	auto arr[10] = { 0 };
+//	return 0;
+//}
+
+
+//范围for循环
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,0 };
+//	for (auto& e : arr)
+//	{
+//		e += 1;
+//	}
+//	for (auto e : arr)
+//	{
+//		cout << e << " ";
+//	}
+//	return 0;
+//}
+
+//void TestFor(int array[])
+//{
+//	for (auto& e : array)
+//		cout << e << endl;
+//}
+
+void func(int)
+{
+	cout << "f(int)" << endl;
+}
+void func(int*)
+{
+	cout << "f(int*)" << endl;
+}
+
+int main()
+{
+	func(0);
+	func(nullptr);
+	func((int*)NULL);
+	return 0;
+}
+
+//int main()
+//{
+//	int* ptr = NULL;
+//	func(ptr);
+//
+//	return 0;
+//}
+
+
+
---
+++ CPP_CODE/8_2.cpp
@@ -0,0 +1,62 @@
+//#include<iostream>
+//
+//using namespace std;
+//string s1("hello worldxxxxxxxxxxxxxxxxxxxxxxxxxxyyyyyyyyyyyyyyyyyy");
+//
+//void test1()
+//{
+//    
+//
+//
+//    string s2(s1);
+//    cout << s2 << endl;
+//
+//    string s3(s1, 6, 5);
+//    cout << s3 << endl;
+//
+//    string s4(s1, 6, 3);
+//    cout << s4 << endl;
+//
+//    string s5(s1, 6);
+//    cout << s5 << endl;
+//
+//    string s6(s1, 6, s1.size() - 6);
+//    cout << s6 << endl;
+//
+//    string s7(10, 'a');
+//    cout << s7 << endl;
+//
+//    string s8(++s7.begin(), --s7.end());
+//    cout << s8 << endl;
+//
+//    s8 = s7;
+//    s8 = "xxx";
+//    s8 = 'y';
+//}
+//
+//void test2()
+//{
+// 
+//    cout << s1.size() << endl;
+//    cout << s1.length() << endl;
+//    cout << s1.capacity() << endl;
+//
+//    s1.clear();
+//    s1 += "ÕÅÈý";
+//    cout << s1.size() << endl;
+//    cout << s1.capacity() << endl;
+//    cout << s1.max_size() << endl;
+//
+//
+//
+//    string filename;
+//    cin >> filename;
+//    FILE* fout = fopen(filename.c_str(), "r");
+//}
+//
+//int main()
+//{
+//    test2();
+//
+//    return 0;
+//}
