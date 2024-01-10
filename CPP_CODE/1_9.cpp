559bc85b704fdf39c04d49ba8d73f52b9dcf63a9
crink1 <1575460882@qq.com>
2024-01-10 21:49:26 +0800

    练习

--- CPP_CODE/1_9.cpp
+++ CPP_CODE/1_9.cpp
@@ -29,14 +29,14 @@
 //    }
 //};
 
-#include<iostream>
-using namespace std;
-
-int main()
-{
-    int a = 0;
-    int b = 0;
-    cin >> a >> b;
-    cout << a + b << endl;
-    return 0;
-}
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//    int a = 0;
+//    int b = 0;
+//    cin >> a >> b;
+//    cout << a + b << endl;
+//    return 0;
+//}
