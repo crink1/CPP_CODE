000eeb9744291d183265aceed9cfaae884959c41
crink1 <1575460882@qq.com>
2024-01-20 23:23:31 +0800

    练习

---
+++ CPP_CODE/1_20.cpp
@@ -0,0 +1,37 @@
+//#include<iostream>
+//#include<string>
+//#include<map>
+//using namespace std;
+//
+//class A
+//{
+//public:
+//	A* GetInstance()
+//	{
+//		return &_a;
+//	}
+//
+//	void add(const string& key, const string& val)
+//	{
+//
+//		_dict[key] = val;
+//	}
+//
+//	void print()
+//	{
+//		for(auto)
+//	}
+//private:
+//	A()
+//	{}
+//	A(const A& aa) = delete;
+//	A& operator=(const A& aa) = delete;
+//	map<string, string> _dict;
+//	int _x;
+//	static A _a;
+//};
+//
+//int main()
+//{
+//	return 0;
+//}
