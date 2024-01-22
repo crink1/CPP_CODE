ad771fcde28a4294e000d3b275c85cb6de93c073
crink1 <1575460882@qq.com>
2024-01-22 23:15:38 +0800

    练习

---
+++ CPP_CODE/1_22.cpp
@@ -0,0 +1,25 @@
+//class Solution {
+//public:
+//    int longestCommonSubsequence(string text1, string text2) {
+//        int m = text1.size();
+//        int n = text2.size();
+//        text1 = " " + text1;
+//        text2 = " " + text2;
+//        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+//        for (int i = 1; i <= m; i++)
+//        {
+//            for (int j = 1; j <= n; j++)
+//            {
+//                if (text1[i] == text2[j])
+//                {
+//                    dp[i][j] = dp[i - 1][j - 1] + 1;
+//                }
+//                else
+//                {
+//                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+//                }
+//            }
+//        }
+//        return dp[m][n];
+//    }
+//};
