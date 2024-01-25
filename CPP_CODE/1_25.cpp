82d924955c1af7d01e3ea4cfa192e7d45ee27d2f
crink1 <1575460882@qq.com>
2024-01-25 22:57:30 +0800

    练习

---
+++ CPP_CODE/1_25.cpp
@@ -0,0 +1,42 @@
+//class Solution {
+//public:
+//    bool isMatch(string s, string p) {
+//        int m = s.size();
+//        int n = p.size();
+//        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
+//        dp[0][0] = true;
+//        for (int j = 1; j <= n; j++)
+//        {
+//            if (p[j - 1] == '*')
+//            {
+//                dp[0][j] = true;
+//            }
+//            else
+//            {
+//                break;
+//            }
+//        }
+//        for (int i = 1; i <= m; i++)
+//        {
+//            for (int j = 1; j <= n; j++)
+//            {
+//                if (s[i - 1] == p[j - 1] && dp[i - 1][j - 1] == true)
+//                {
+//                    dp[i][j] = true;
+//                }
+//                else if (p[j - 1] == '?')
+//                {
+//                    if (dp[i - 1][j - 1] == true)
+//                    {
+//                        dp[i][j] = true;
+//                    }
+//                }
+//                else if (p[j - 1] == '*')
+//                {
+//                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+//                }
+//            }
+//        }
+//        return dp[m][n];
+//    }
+//};
