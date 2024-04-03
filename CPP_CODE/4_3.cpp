4bb423f2fd04dce0cdc157a5f63927f9f9ac3e4b
crink1 <1575460882@qq.com>
2024-04-03 22:58:37 +0800

    练习

---
+++ CPP_CODE/4_3.cpp
@@ -0,0 +1,44 @@
+//#include <bits/stdc++.h>
+//#define int long long
+//#define endl '\n'
+//#define INF 0x3f3f3f3f
+//using namespace std;
+//
+//void solve()
+//{
+//    int k = 0;
+//    cin >> k;
+//    string str;
+//    char a, b;
+//    cin >> str >> a >> b;
+//    int cnt = 0;
+//    for (int i = 0; i < str.size(); i++)
+//    {
+//        if (a == str[i])
+//        {
+//            for (int j = i + k - 1; j < str.size(); j++)
+//            {
+//                if (b == str[j])
+//                {
+//                    cnt++;
+//                }
+//            }
+//        }
+//
+//    }
+//    cout << cnt << endl;
+//
+//}
+//
+//signed main()
+//{
+//    ios::sync_with_stdio(0);
+//    cin.tie(0);
+//    cout.tie(0);
+//    int t = 1;
+//    while (t--)
+//    {
+//        solve();
+//    }
+//    return 0;
+//}
