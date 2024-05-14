c4668fc70b5052e2c3e5ab13ce18bfd7c50d5061
crink1 <1575460882@qq.com>
2024-05-14 22:14:17 +0800

    练习

---
+++ CPP_CODE/5_14.cpp
@@ -0,0 +1,18 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    string s1, s2;
+//    while (cin >> s1 >> s2)
+//    {
+//        int cnt = 0;
+//        int pos = -1;
+//        while ((pos = s1.find(s2, pos + 1)) != string::npos)
+//        {
+//            cnt++;
+//        }
+//        cout << cnt << endl;
+//
+//
+//    }
+//}
