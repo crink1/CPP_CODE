46357644b1e6e468df5749870ded75940bc71785
crink1 <1575460882@qq.com>
2024-05-15 22:23:26 +0800

    练习

---
+++ CPP_CODE/5_15.cpp
@@ -0,0 +1,38 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main() {
+//    bool flag = false;
+//    string s;
+//    int n = 0;
+//    while (cin >> n)
+//    {
+//        for (int i = 1; i <= n; i++)
+//        {
+//            
+//            getline(cin, s);
+//            if (s.empty())
+//            {
+//                continue;
+//            }
+//            cout << s << 111;
+//        }
+//        cout << endl;
+//
+//    }
+//}
+//
+//#include<stdio.h>
+//int main()
+//{
+//    __int64 a[95] = { 1,2 };
+//    int n, i;
+//    for (i = 2; i < 91; i++)
+//        a[i] = a[i - 1] + a[i - 2];
+//    while (scanf("%d", &n) != EOF)
+//    {
+//        printf("%I64d\n", a[n - 1]);
+//    }
+//    return 0;
+//}
