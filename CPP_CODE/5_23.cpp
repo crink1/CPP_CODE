b8989913f3240f884904ee06808328f65b3275de
crink1 <1575460882@qq.com>
2024-05-23 22:57:51 +0800

    练习

---
+++ CPP_CODE/5_23.cpp
@@ -0,0 +1,44 @@
+//// write your code here cpp
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//int main() {
+//    int n;
+//    while (cin >> n) {
+//        vector<int> height(n, 0);
+//        for (int i = 0; i < n; i++) {
+//            cin >> height[i];
+//        }
+//
+//        vector<int> f(n, 1);
+//        int result = 1;
+//        for (int i = 1; i < n; i++) {
+//            for (int j = 0; j < i; j++) {
+//                if (height[i] > height[j]) {
+//                    f[i] = max(f[i], f[j] + 1);
+//                }
+//            }
+//            result = max(result, f[i]);
+//        }
+//
+//        cout << result << endl;
+//    }
+//}
+
+
+
+//#include<stdio.h>
+//int main(void)
+//{
+//    long long der[21] = { 0, 0, 1 };
+//    int i;
+//    for (i = 3; i < 21; i++) {
+//        der[i] = (i - 1) * (der[i - 2] + der[i - 1]);
+//    }
+//
+//    int n;
+//    while (scanf("%d", &n) != EOF) {
+//        printf("%lld\n", der[n]);
+//    }
+//    return 0;
+//}
