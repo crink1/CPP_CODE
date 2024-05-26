5e209eca3d8aed7b0b722f133c184a349041b42f
crink1 <1575460882@qq.com>
2024-05-26 23:03:46 +0800

    练习

---
+++ CPP_CODE/5_26.cpp
@@ -0,0 +1,19 @@
+//#include <iostream>
+//#include <thread>
+//using namespace std;
+//union test
+//{
+//	int x;
+//	char c;
+//};
+//
+//int main()
+//{
+//	test t1;
+//	t1.x = 1;
+//	if (t1.c == 1)
+//	{
+//		cout << 11;
+//	}
+//	return 0;
+}
