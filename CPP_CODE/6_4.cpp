5ca78813de9ceea9228f560022fe90ce7930c67d
crink1 <1575460882@qq.com>
2024-06-04 22:40:16 +0800

    练习

---
+++ CPP_CODE/6_4.cpp
@@ -0,0 +1,23 @@
+//class Solution {
+//public:
+//    int removeDuplicates(vector<int>& nums) {
+//        int n = nums.size();
+//        if (n <= 2)
+//        {
+//            return n;
+//        }
+//
+//        int s = 2;
+//        int f = 2;
+//        while (f < n)
+//        {
+//            if (nums[s - 2] != nums[f])
+//            {
+//                nums[s] = nums[f];
+//                s++;
+//            }
+//            f++;
+//        }
+//        return s;
+//    }
+//};
