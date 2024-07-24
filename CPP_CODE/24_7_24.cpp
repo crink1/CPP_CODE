1e94d8b1642ebcb92f23629591402356c1131776
crink1 <1575460882@qq.com>
2024-07-24 20:26:31 +0800

    练习

---
+++ CPP_CODE/24_7_24.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    long long n = 0;
+    cin >> n;
+    long long x = sqrt(n);
+    cout << x * x << endl;
+    return 0;
+}
+// 64 Î»Êä³öÇëÓÃ printf("%lld")
