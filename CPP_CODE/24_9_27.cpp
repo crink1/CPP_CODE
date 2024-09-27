2618602b10ef3f3ef8eb40ea45e7f5f0da850477
crink1 <1575460882@qq.com>
2024-09-27 23:26:31 +0800

    练习

---
+++ CPP_CODE/24_9_27.cpp
@@ -0,0 +1,44 @@
+//class Solution {
+//public:
+//    bool lemonadeChange(vector<int>& bills) {
+//        int five = 0;
+//        int ten = 0;
+//        for (auto& i : bills)
+//        {
+//            if (i == 5)
+//            {
+//                five++;
+//            }
+//            else if (i == 10)
+//            {
+//                if (five)
+//                {
+//                    five--;
+//                    ten++;
+//                }
+//                else
+//                {
+//                    return false;
+//                }
+//            }
+//            else
+//            {
+//                if (five && ten)
+//                {
+//                    five--;
+//                    ten--;
+//                }
+//                else if (five >= 3)
+//                {
+//                    five -= 3;
+//                }
+//                else
+//                {
+//                    return false;
+//                }
+//            }
+//
+//        }
+//        return true;
+//    }
+//};
