d943d4cad0328280aa7b6e5c99618f4aa88c9ab0
crink1 <1575460882@qq.com>
2024-10-26 23:10:19 +0800

    练习

---
+++ CPP_CODE/24_10_26.cpp
@@ -0,0 +1,25 @@
+//class Solution {
+//public:
+//    int largestRectangleArea(vector<int>& heights) {
+//        stack<int> st;
+//        st.push(0);
+//        heights.insert(heights.begin(), 0);
+//        heights.push_back(0);
+//
+//        int ret = 0;
+//
+//        for (int i = 1; i < heights.size(); i++)
+//        {
+//            while (heights[i] < heights[st.top()])
+//            {
+//                int m = st.top();
+//                st.pop();
+//                int h = heights[m];
+//                int w = i - st.top() - 1;
+//                ret = max(ret, h * w);
+//            }
+//            st.push(i);
+//        }
+//        return ret;
+//    }
+//};
