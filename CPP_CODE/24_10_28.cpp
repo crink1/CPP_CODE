76418e91f3332975c3694074331b4ef5894f0460
crink1 <1575460882@qq.com>
2024-10-28 22:48:51 +0800

    练习

---
+++ CPP_CODE/24_10_28.cpp
@@ -0,0 +1,44 @@
+//#include<iostream>
+//using namespace std;
+//
+//class A
+//{
+//public:
+//	
+//	virtual void show()
+//	{
+//		cout << "****" << endl;
+//	}
+//};
+//
+//class B : public A
+//{
+//public:
+//	virtual void show()
+//	{
+//		cout << "$$$$" << endl;
+//	}
+//};
+//
+//class C : public A
+//{
+//public:
+//	virtual void show()
+//	{
+//		cout << "&&&&" << endl;
+//	}
+//};
+//
+//int main()
+//{
+//	B b1;
+//	C c1;
+//
+//	A& a1 = b1;
+//	a1.show();
+//	A& a2 = c1;
+//	a2.show();
+//
+//
+//	return 0;
+//}
